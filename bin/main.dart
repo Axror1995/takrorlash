b58837fee506abfffd073c42d2d3ebef93d10755
aror raximmov <axrorraximov108@gmail.com>
2022-08-22 11:26:01 +0500

    takrorlash

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,35 @@
 void main (){
-  double x;
-  x=45656.445;
-  print(x);
+    String a;
+  int w;
+  int e;
+  int r;
+  int t;
+  String s;
+  int y;
+  int u;
+  String d;
+  int i;
+  int o;
+  a = "yil";
+  w = 1;
+  e = 9;
+  r = 9;
+  t = 5;
+  s = "oy";
+  y = 0;
+  u = 8;
+  d = "sana";
+  i = 1;
+  o = 5;
+  print(a); 
+  print(w);
+  print(e);
+  print(r);
+  print(t);
+  print(s);
+  print(y);
+  print(u);
+  print(d);
+  print(i);
+  print(o);
 }
